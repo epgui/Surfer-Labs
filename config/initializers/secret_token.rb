cf478f930ac4bc3759620b2c814e003c660f9ae3
Guillaume Pelletier <guigui.p@gmail.com>
2012-10-31 23:17:38 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surfer::Application.config.secret_token = 'cf4a519014089a9058515ea7e397c8fc2678d026dd0eaa93d413fbaa06fc39f978c1f7eef57054f697dc13660b33c86d96211aac6fc6896270424262e1c10862'
